d59372ab34fb7e3b94b5688da91c17a86f9d9558
Bryn Loftness <brynchristineloftness@yahoo.com>
2022-12-19 16:27:00 -0500

    rebuilding

--- _build/jupyter_execute/script_link_page.py
+++ _build/jupyter_execute/script_link_page.py
@@ -14,9 +14,9 @@
 # :align: center
 # ```
 # 
-# #### Before beginning, you must download our open-access dataset of ChAMP data or collect your own data using the ChAMP mobile application. 
+# #### Before beginning, you must download the real-world example files from our ChAMP data or collect your own data using the ChAMP mobile application. 
 # 
-# *need to add link:* To download the open access dataset already collected by our team, go [here](). 
+# To download the open access dataset already collected by our team, go [here](https://drive.google.com/drive/folders/1CDAYxIMWCOtvndpSXO0XaQLqx-Gw0nl7?usp=sharing). Save this data in this folder to the home directory of your Google Drive in a folder called "champ_data" to use directly with the ChAMP interactive script. Ensure each action log file is in the format of S###.actionLog.projectmHealth.csv and each data log file is in the format of S###.dataLog.projectmHealth.csv.
 # 
 # To request download access for the ChAMP mobile application to collect your own dataset, please fill out [this form](https://forms.office.com/r/PEfwybkiAM) or scan this QR code.
 # 
@@ -27,8 +27,6 @@
 # :align: center
 # ```
 # 
-# To access the script with step-by-step instructions, go [here](https://colab.research.google.com/drive/1g6yjky4avTAsZMPUO4AFukOi75jci2DO) or click the rocket ship icon in the upper right corner of this page and select from the drop-down to open in Google Colab.
+# To access the script with step-by-step instructions, go [here](https://colab.research.google.com/drive/1g6yjky4avTAsZMPUO4AFukOi75jci2DO) or click the rocket ship icon in the upper right corner of this page and select from the drop-down to open in Google Colab. Once you are on the script page, under the top options bar click "Copy to Drive". Once you've made the folder with data (champ_data) in the home directory of your drive, on the top click "Runtime" -> "Run All". You will be prompted to connect your google drive to the script. Once the program has completed running (may take a minute or two depending on the number of files), one CSV will be downloaded onto your computer with features extracted for each subject.
 # 
 # **If you have any questions downloading the data or running this interactive script, please contact Bryn Loftness at bryn.loftness@uvm.edu**. 
-
-# 
